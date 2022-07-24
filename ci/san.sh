50c61fc202b60e25fc6b784179241344dbcd7ecc
Taiki Endo <te316e89@gmail.com>
2022-07-24 19:58:05 +0900

    wip

--- ci/san.sh
+++ ci/san.sh
@@ -9,39 +9,39 @@ fi
 
 rustup component add rust-src
 
-# Run address sanitizer
-# TODO: Once `cfg(sanitize = "..")` is stable, replace
-# `cfg(crossbeam_sanitize)` with `cfg(sanitize = "..")` and remove
-# `--cfg crossbeam_sanitize`.
-cargo clean
-RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
-    cargo test --all --release --target x86_64-unknown-linux-gnu --tests \
-    --exclude crossbeam-skiplist --exclude benchmarks -- --test-threads=1
+# # Run address sanitizer
+# # TODO: Once `cfg(sanitize = "..")` is stable, replace
+# # `cfg(crossbeam_sanitize)` with `cfg(sanitize = "..")` and remove
+# # `--cfg crossbeam_sanitize`.
+# cargo clean
+# RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
+#     cargo test --all --release --target x86_64-unknown-linux-gnu --tests \
+#     --exclude crossbeam-skiplist --exclude benchmarks -- --test-threads=1
 
-# There are memory leaks in crossbeam-skiplist.
-# https://github.com/crossbeam-rs/crossbeam/issues/614
-cargo clean
-RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
-    cargo test --release --target x86_64-unknown-linux-gnu \
-    -p crossbeam-skiplist --test map --test set
-cargo clean
-ASAN_OPTIONS="detect_leaks=0" \
-    RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
-    cargo test --release --target x86_64-unknown-linux-gnu \
-    -p crossbeam-skiplist --tests
+# # There are memory leaks in crossbeam-skiplist.
+# # https://github.com/crossbeam-rs/crossbeam/issues/614
+# cargo clean
+# RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
+#     cargo test --release --target x86_64-unknown-linux-gnu \
+#     -p crossbeam-skiplist --test map --test set
+# cargo clean
+# ASAN_OPTIONS="detect_leaks=0" \
+#     RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
+#     cargo test --release --target x86_64-unknown-linux-gnu \
+#     -p crossbeam-skiplist --tests
 
-cargo clean
-RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
-    cargo run \
-    --release \
-    --target x86_64-unknown-linux-gnu \
-    --example sanitize \
-    --manifest-path crossbeam-epoch/Cargo.toml
+# cargo clean
+# RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=address --cfg crossbeam_sanitize" \
+#     cargo run \
+#     --release \
+#     --target x86_64-unknown-linux-gnu \
+#     --example sanitize \
+#     --manifest-path crossbeam-epoch/Cargo.toml
 
-# Run memory sanitizer
-cargo clean
-RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=memory --cfg crossbeam_sanitize" \
-    cargo test -Z build-std --all --release --target x86_64-unknown-linux-gnu --tests --exclude benchmarks -- --test-threads=1
+# # Run memory sanitizer
+# cargo clean
+# RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=memory --cfg crossbeam_sanitize" \
+#     cargo test -Z build-std --all --release --target x86_64-unknown-linux-gnu --tests --exclude benchmarks -- --test-threads=1
 
 # Run thread sanitizer
 cargo clean
